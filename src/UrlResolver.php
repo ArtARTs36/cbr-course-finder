35cac751b59d5dc5f8608bc7d4f918ded4331839
artarts36 <temicska99@mail.ru>
2022-11-26 11:57:02 +0300

    add test

--- src/UrlResolver.php
+++ src/UrlResolver.php
@@ -6,7 +6,6 @@ final class UrlResolver implements Contracts\UrlResolver
 {
     private const DEFAULT_BASE_URL = 'https://www.cbr-xml-daily.ru/';
     private const DATE_FORMAT = 'Y/m/d';
-    private const URL_CURRENT_DATE = self::DEFAULT_BASE_URL . 'daily_json.js';
 
     public function __construct(
         private string $baseUrl = self::DEFAULT_BASE_URL,
@@ -19,7 +18,7 @@ final class UrlResolver implements Contracts\UrlResolver
         $date = $date->format(self::DATE_FORMAT);
 
         return $date === date(self::DATE_FORMAT) ?
-            self::URL_CURRENT_DATE :
+            $this->baseUrl . '/daily_json.js' :
             $this->baseUrl . "/archive/{$date}/daily_json.js";
     }
 }
